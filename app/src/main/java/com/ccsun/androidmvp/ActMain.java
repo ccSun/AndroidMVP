13096e4b67bf293446393de9beea7bdfa057721c
nsnv.CCSUN <ccSun7758@126.com>
2016-07-22 16:43:32 +0800

    Init Proj.

---
+++ app/src/main/java/com/ccsun/androidmvp/ActMain.java
@@ -0,0 +1,16 @@
+package com.ccsun.androidmvp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ccsun on 7/22/16.
+ */
+public class ActMain extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_main);
+    }
+}
